27b229be33fa47ed31f3c654db684ce239606b0c
dlaboy <diego.laboy1@upr.edu>
2024-12-03 00:58:03 -0400

    removing old add

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,8 +9,6 @@ export  function Home() {
   return (
     <>
     <MyNav/>
-    {/* <Banner/> */}
-    <iframe src="//www.highperformanceformat.com/41f36f890b09799367092d025b677bce/invoke.js" height={60} width={468} ></iframe>
     <MyHead/>
     
     </>
