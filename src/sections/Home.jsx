c2aecaa0edded03ab1de9ada5480200be3e81814
dlaboy <diego.laboy1@upr.edu>
2024-12-03 00:17:59 -0400

    banner

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -9,7 +9,8 @@ export  function Home() {
   return (
     <>
     <MyNav/>
-    <Banner/>
+    {/* <Banner/> */}
+    <iframe src="//www.highperformanceformat.com/41f36f890b09799367092d025b677bce/invoke.js" height={60} width={468} frameborder="0"></iframe>
     <MyHead/>
     
     </>
