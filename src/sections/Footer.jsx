a1d7ed32f09f687218258b17ba555cad99415b96
dlaboy <diego.laboy1@upr.edu>
2024-12-02 22:55:51 -0400

    Updates

--- src/sections/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Footer() {
-  return (
-    <div className="text-center fw-bolder bg-dark  pt-3 pb-3 w-100">
-    <h1 className="fw-bolder text-light">DL</h1>
-  </div>
-  )
-}
