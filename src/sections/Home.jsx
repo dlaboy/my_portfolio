2bd2b414ebf0550e9aa4d89fdf81a10e85125774
dlaboy <diego.laboy1@upr.edu>
2024-12-02 23:41:09 -0400

    adding banner

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,11 +1,15 @@
 import { MyNav } from "../components/MyNav";
 import { MyHead } from "../components/myHead";
+import { Banner } from "../components/Banner"
+
+
 
 
 export  function Home() {
   return (
     <>
     <MyNav/>
+    <Banner/>
     <MyHead/>
     
     </>
